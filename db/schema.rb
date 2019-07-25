0bbe73d31c46ebe1f1e65d683140fad377dd5730
Travis Exline <t.exline22@gmail.com>
2019-07-25 15:20:16 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212204456) do
+ActiveRecord::Schema.define(version: 20190725151353) do
+
+  create_table "passengers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "passgener_id"
+    t.integer  "taxi_id"
+  end
 
   create_table "taxis", force: :cascade do |t|
     t.datetime "created_at", null: false
